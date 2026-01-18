10dfcb575ebe598857fd530fbf91b75c1c51d4a2
Poojakk112 <poojakochalapurmath@gmail.com>
2026-01-18 15:24:12 +0530

    Add Number Game Java program

---
+++ Task1_NumberGame/Task1_NumberGame/NumberGame.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int number = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Welcome to Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess > number) {
+                System.out.println("Too High!");
+            } else if (guess < number) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
+            }
+        } while (guess != number);
+
+        sc.close();
+    }
+}
